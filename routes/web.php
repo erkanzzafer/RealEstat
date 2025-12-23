88dd3cef14dc9888ab33b4db578173a8116641b5
Zafer Erkan <83276774+erkanzzafer@users.noreply.github.com>
2025-12-23 20:27:50 +0300

    admin index template parçalama ve logout işlemi yapıldı

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,9 @@ Route::middleware('auth')->group(function () {
 //Admin Route
 Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+    Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
+
+
 });
 
 //Agent Route
