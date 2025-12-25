b23850003ea8775f6a0c2a78e698c895e43966df
Zafer Erkan <83276774+erkanzzafer@users.noreply.github.com>
2025-12-25 19:55:29 +0300

    admin profile sayfası hazırlandı

--- routes/web.php
+++ routes/web.php
@@ -36,6 +36,7 @@ Route::middleware('auth')->group(function () {
 Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
     Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
+    Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
 
 
 });
@@ -46,6 +47,6 @@ Route::middleware(['auth', 'role:agent'])->group(function () {
 });
 
 
- Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
+Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
 
 require __DIR__ . '/auth.php';
