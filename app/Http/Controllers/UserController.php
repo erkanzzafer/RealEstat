15a043a993e522f5dcfc75df71c2e01dccbfdd3f
Zafer Erkan <83276774+erkanzzafer@users.noreply.github.com>
2025-12-29 20:41:54 +0300

    user profil setting tamamlandı

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use finfo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -11,4 +13,36 @@ class UserController extends Controller
     {
         return view('frontend.index' );
     }
+
+    public function userProfile(){
+        $id=Auth::user()->id;
+        $userData= User::find($id);
+        return view ('frontend.dashboard.editProfile',compact('userData'));
+    }
+
+    public function userProfileStore (Request $request){
+        $id = Auth::user()->id;
+        $data = User::find($id);
+
+        $data->username = $request->username;
+        $data->name = $request->name;
+        $data->email = $request->email;
+        $data->phone = $request->phone;
+        $data->address = $request->address;
+
+        if ($request->file('photo')) {
+            $file = $request->file('photo');
+            @unlink(public_path('upload/user_images/' . $data->photo));
+            $filename = date('YmdHi') . $file->getClientOriginalName();
+            $file->move(public_path('upload/user_images'), $filename);
+            $data->photo = $filename;
+        }
+        $data->save();
+        $notification = array(
+            'message' => 'User Profile Updated Successfully',
+            'alert-type' => 'success'
+        );
+        return redirect()->back()->with($notification);
+    }
+
 }
--- resources/views/frontend/dashboard/editProfile.blade.php
+++ resources/views/frontend/dashboard/editProfile.blade.php
@@ -1,7 +1,7 @@
 @extends('frontend.dashboard')
 @section('index')
-
-        <!--Page Title-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+ <!--Page Title-->
         <section class="page-title centred" style="background-image: url(frontend/assets/images/background/page-title-5.jpg);">
             <div class="auto-container">
                 <div class="content-box clearfix">
@@ -17,15 +17,13 @@
 
 
         <!-- sidebar-page-container -->
-        <section class="sidebar-page-container blog-details sec-pad-2">
+<section class="sidebar-page-container blog-details sec-pad-2">
             <div class="auto-container">
                 <div class="row clearfix">
                     
 
-             @php
-                 $id=Auth::user()->id;
-                 $userData = App\Models\User::find($id);
-             @endphp
+
+
 
 
 
@@ -40,20 +38,22 @@
                     <div class="post-inner">
                         <div class="post">
                             <figure class="post-thumb"><a href="blog-details.html">
-       <img src="{{ asset('upload/user_images/' . ($userData->photo ?? 'no_image.jpg')) }}" alt=""></a></figure>
+       <img src="{{ asset('upload/user_images/'.($userData->photo ?? 'no_image.jpg')) }}" alt=""></a></figure>
         <h5><a href="blog-details.html">{{ $userData->name }} </a></h5>
-         <p>{{ $userData->email }} </p>
+         <p>{{$userData->email}} </p>
                         </div> 
                     </div>
                 </div> 
        
         <div class="sidebar-widget category-widget">
             <div class="widget-title">
+                
             </div>
-            @include('frontend.dashboard.sidebar')
-           </div>                
-         </div>
-      </div>
+                @include('frontend.dashboard.sidebar')
+          </div> 
+                         
+                        </div>
+                    </div>
 
 
 
@@ -64,64 +64,47 @@
                                 <div class="inner-box">
                                     
                                     <div class="lower-content">
-                                        <h3>Including Animation In Your Design System.</h3>
-                                        <ul class="post-info clearfix">
-                                            <li class="author-box">
-                                                <figure class="author-thumb"><img src="{{ asset('frontend/assets/images/news/author-1.jpg') }}" alt=""></figure>
-                                              
-                                            </li>
-                                        
-                                        </ul>
+                                       
                                       
           
+ <form  enctype="multipart/form-data" action="{{route('user.profile.store')}}" method="post" class="default-form">
+    @csrf
+        <div class="form-group">
+            <label>Username</label>
+            <input type="text" name="username" value="{{ $userData->username }}">
+        </div>
+        <div class="form-group">
+            <label>Name</label>
+            <input type="text" name="name" value="{{ $userData->name }}">
+        </div>
+        <div class="form-group">
+            <label>Email</label>
+            <input type="email" name="email" value="{{ $userData->email }}">
+        </div>
+        <div class="form-group">
+            <label>Phone</label>
+            <input type="text" name="phone" value="{{ $userData->phone }}">
+        </div>
+        <div class="form-group">
+            <label>Address</label>
+            <input type="text" name="address" value="{{ $userData->address }}">
+        </div>
+  <div class="form-group">
+            <label for="formFile" class="form-label">Default file input example</label>
+  <input class="form-control" name="photo" type="file" id="image" >
+        </div>
 
 
-<div class="row">
-<div class="col-lg-4">
-    <div class="card-body" style="background-color: #1baf65;">
-    <h1 class="card-title" style="color: white; font-weight: bold;">0</h1>
-    <h5 class="card-text"style="color: white;"> Approved properties</h5>
-    
-  </div>
-</div>
-
-<div class="col-md-4">
-    <div class="card-body" style="background-color: #ffc107;">
-    <h1 class="card-title" style="color: white; font-weight: bold; ">0</h1>
-    <h5 class="card-text"style="color: white;"> Pending approve properties</h5>
-    
-  </div>
-</div>
+        
+  <div class="form-group">
+              <img id="showImage" src="{{ asset('upload/user_images/'.($userData->photo ?? 'no_image.jpg')) }}" style="width: 100px; height: 100px;" alt="">
+        </div>
 
 
-<div class="col-md-4">
-    <div class="card-body" style="background-color: #002758;">
-    <h1 class="card-title" style="color: white; font-weight: bold;">0</h1>
-    <h5 class="card-text"style="color: white; "> Rejected properties</h5>
-    
-  </div>
-</div>
-    
-</div> 
-
-                                    </div>
-                                </div>
-                            </div>
-                             
-                            
-                        </div>
-
-
-    <div class="blog-details-content">
-                            <div class="news-block-one">
-                                <div class="inner-box">
-                                    
-                                    <div class="lower-content">
-                                        <h3>Activity Logs</h3>
-                                      <hr>
-                                      
-          
-
+        <div class="form-group message-btn">
+            <button type="submit" class="theme-btn btn-one">Save Changes </button>
+        </div>
+    </form>
 
  
 
@@ -132,17 +115,13 @@
                             
                         </div>
 
-
-
-
-
-
+ 
                     </div> 
 
 
                 </div>
             </div>
-        </section>
+</section>
         <!-- sidebar-page-container -->
 
         <!-- subscribe-section -->
@@ -170,5 +149,17 @@
             </div>
         </section>
         <!-- subscribe-section end -->
+        <script type="text/javascript">
 
+  $(document).ready(function(){
+    $('#image').change(function(e){
+      var reader=new FileReader();
+      reader.onload = function(e){
+        $('#showImage').attr('src',e.target.result);
+      }
+      reader.readAsDataURL(e.target.files['0']);
+    });
+  });
+
+</script>
 @endsection
